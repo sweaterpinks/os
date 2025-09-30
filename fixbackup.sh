0ee882c2eb245cdcc49e23195ead651960784058
sweaterpinks <cirosumpena@gmail.com>
2025-09-30 09:05:00 +0700

    Update fixbackup.sh

--- fixbackup.sh
+++ fixbackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/gazzent/os/main/"
+REPO="https://raw.githubusercontent.com/kingstorevpn/os/main/"
 apt install rclone
 printf "q\n" | rclone config
 wget -O /root/.config/rclone/rclone.conf "${REPO}install/rclone.conf"
@@ -10,5 +10,5 @@ cd
 rm -rf wondershaper
 wget -q ${REPO}install/limit.sh && chmod +x limit.sh && ./limit.sh
     
-rm -f /root/fixbackup.sh
+rm -f /root/set-br.sh
 rm -f /root/limit.sh
